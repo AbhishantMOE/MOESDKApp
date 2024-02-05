dc17a925aac897dac25b398a602f47fed903601b
AbhishantMOE <abhishant.singh@moengage.com>
2024-02-05 14:24:54 +0530

    first commit after tutorial gfg

---
+++ app/src/main/java/com/example/sdkdemoapp/MyApplication.kt
@@ -0,0 +1,50 @@
+package com.example.sdkdemoapp
+
+import android.app.Application
+import com.moengage.core.DataCenter
+import com.moengage.core.LogLevel
+import com.moengage.core.MoECoreHelper
+import com.moengage.core.MoEngage
+import com.moengage.core.analytics.MoEAnalyticsHelper
+import com.moengage.core.config.FcmConfig
+import com.moengage.core.config.LogConfig
+import com.moengage.core.config.TrackingOptOutConfig
+import com.moengage.core.enableAdIdTracking
+import com.moengage.core.enableAndroidIdTracking
+import com.moengage.core.model.AppStatus
+
+
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        val trackingOptOutConfig = TrackingOptOutConfig(
+            isCarrierTrackingEnabled = true,
+            isDeviceAttributeTrackingEnabled = true,
+
+            )
+        //
+        val moEngage = MoEngage.Builder(this, "OXTAVQZDWWAROL2ESF8FWE8G", DataCenter.DATA_CENTER_1)
+            .configureLogs(LogConfig(LogLevel.VERBOSE, false))
+            //.configureTrackingOptOut(trackingOptOutConfig)
+            .configureFcm(
+                FcmConfig(false)
+            )
+            .build()
+        MoEngage.initialiseDefaultInstance(moEngage)
+//        enableAndroidIdTracking(applicationContext)
+//        enableAdIdTracking(applicationContext)
+//        MoEAnalyticsHelper.setAppStatus(applicationContext, AppStatus.INSTALL)
+//
+//        MoEAnalyticsHelper.setUniqueId(applicationContext, "65b8d313ef6641a0cd145c59")
+//
+//        MoEAnalyticsHelper.setFirstName(applicationContext,"Akatsuki")
+
+
+
+
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/sdkdemoapp/MyFirebaseMessagingService.kt
@@ -0,0 +1,87 @@
+package com.example.sdkdemoapp
+
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.widget.RemoteViews
+import android.widget.RemoteViews.RemoteView
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import okhttp3.internal.notify
+
+const val channelId = "Notification_channel"
+const val channelName= "com.example.sdkdemoapp"
+
+@SuppressLint("MissingFirebaseInstanceTokenRefresh")
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+    // generate the notification
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        if(remoteMessage.notification != null){
+        generateNotification(
+            remoteMessage.notification!!.title!!,
+            remoteMessage.notification!!.body!!
+        )
+    }
+    }
+
+    fun getRemoteView(title:String,message:String): RemoteViews{
+        val remoteView = RemoteViews("com.example.sdkdemoapp",R.layout.notification )
+
+        remoteView.setTextViewText(R.id.title,title)
+        remoteView.setTextViewText(R.id.message,message)
+        remoteView.setImageViewResource(R.id.app_logo,R.drawable.smalllogo)
+
+        return remoteView
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun generateNotification(title:String, message:String){
+
+        // to redirect to app
+        val intent = Intent(this,MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)  // clears all the activity in activity stack and puts this on top
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            intent,
+            PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE
+        )
+
+        //channel id, channel name
+        var builder : NotificationCompat.Builder = NotificationCompat
+            .Builder(applicationContext, channelId)
+            .setSmallIcon(R.drawable.smalllogo)
+            .setAutoCancel(true)
+            .setVibrate(longArrayOf(1000,1000,1000,1000))
+            .setOnlyAlertOnce(true).setContentIntent(pendingIntent)
+
+        builder = builder.setContent(getRemoteView(title,message))
+
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE,) as NotificationManager
+
+        val notificationChannel = NotificationChannel(channelId, channelName,NotificationManager.IMPORTANCE_HIGH)
+
+        notificationManager.createNotificationChannel(notificationChannel)
+
+        notificationManager.notify(0,builder.build())
+
+
+
+
+
+    }
+
+    // attach the notification created with the custom layout
+
+    //show the notification
+
+}
